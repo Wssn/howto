680821e9fbe393e2470a5188ba26364651215b15
Oğuzcan Köse <oguzcan.kose@hotmail.com>
2018-06-13 17:18:07 +0100

    rebase

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -1,35 +1,38 @@
 <template>
 <div>
-  <!-- <h1>slug {{ loadedPost.id }}</h1> -->
+  <!-- <h1>{{ loadedPost.id }}</h1> -->
   <!-- <div key="loadedPost.title">  -->
   {{ loadedPost.author }}
   {{ loadedPost.title }}
-  
   </div>
 </template>
 
 <script>
 import Vuex from "vuex"
 import axios from "axios"
+
 export default {
   data() {
     return {
+      isLoading: false,
       loadedPost: {}
     }
   },
-  async asyncData({app, params}) {
-    const obj = await app.$axios.$get('https://howto-a9089.firebaseio.com/posts.json?orderBy="slug"&equalTo=' + '"' + params.slug + '"' + '&print=pretty');
-    const id = Object.keys(obj)[0];
-    const loadedPost = obj[id];
-    return {
-      loadedPost
-    };
+  async asyncData({params}) {
+    return axios.get('https://howto-a9089.firebaseio.com/posts.json?orderBy="slug"&equalTo=' + '"' + params.slug + '"' + '&print=pretty').then((res) => {
+        const obj = res.data;
+        const id = Object.keys(obj)[0];
+        const loadedPost = obj[id];
+      return {
+        loadedPost
+      };
+    })
   },
-    head() {
+  head() {
       return {
         title: this.loadedPost.title,
       }
-    } 
+  } 
   }
  
 </script>
