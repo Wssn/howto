140f6cb511f88c6753cd8812602132ca09e7d504
Oğuzcan Köse <oguzcan.kose@hotmail.com>
2018-06-13 17:26:21 +0100

    rebase

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -19,7 +19,8 @@ export default {
     }
   },
   async asyncData({params}) {
-    return axios.get('https://howto-a9089.firebaseio.com/posts.json?orderBy="slug"&equalTo=' + '"' + params.slug + '"' + '&print=pretty').then((res) => {
+    return axios.get('https://howto-a9089.firebaseio.com/posts.json?orderBy="slug"&equalTo=' + '"' + params.slug + '"' + '&print=pretty')
+    .then((res) => {
         const obj = res.data;
         const id = Object.keys(obj)[0];
         const loadedPost = obj[id];
@@ -30,7 +31,7 @@ export default {
   },
   head() {
       return {
-        title: this.loadedPost.title,
+        title: this.loadedPost.title
       }
   } 
   }
