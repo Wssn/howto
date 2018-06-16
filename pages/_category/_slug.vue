57b0c959079a17b33ccdc47255e9e41491c3bdbb
Oğuzcan Köse <oguzcan.kose@hotmail.com>
2018-06-17 00:07:19 +0100

    detail page completed

--- pages/_category/_slug.vue
+++ pages/_category/_slug.vue
@@ -1,9 +1,13 @@
 <template>
-<div>
-  <!-- <h1>{{ loadedPost.id }}</h1> -->
-  <!-- <div key="loadedPost.title">  -->
-  {{ loadedPost.author }}
-  {{ loadedPost.title }}
+  <div>
+    <h1>{{ result.title }}</h1>
+      {{ result.category }}
+      <hr>
+      <div v-for="step in result.steps" :key="step.id"> 
+        {{ step.title }} <br>
+        {{ step.desc }}
+        <hr>
+      </div>
   </div>
 </template>
 
@@ -12,28 +16,32 @@ import Vuex from "vuex"
 import axios from "axios"
 
 export default {
-  data() {
-    return {
-      isLoading: false,
-      loadedPost: {}
-    }
-  },
   async asyncData({params}) {
     return axios.get('https://howto-a9089.firebaseio.com/posts.json?orderBy="slug"&equalTo=' + '"' + params.slug + '"' + '&print=pretty')
     .then((res) => {
         const obj = res.data;
         const id = Object.keys(obj)[0];
-        const loadedPost = obj[id];
-      return {
-        loadedPost
-      };
+        const result = obj[id];
+        return {
+          result
+        }
     })
   },
   head() {
-      return {
-        title: this.loadedPost.title
-      }
-  } 
+    return {
+      title: this.result.title,
+      meta: [{
+        hid: `description`,	
+        name: 'description',	
+        content: this.result.meta.description	
+      },	
+      {	
+        hid: `keywords`,	
+        name: 'keywords',	
+        keywords: this.result.meta.keywords	
+      }]
+    } 
   }
+}
  
 </script>
