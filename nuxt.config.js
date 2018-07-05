faba605f783fda99b48269c925e1527a69eb7030
Oğuzcan Köse <oguzcan.kose@hotmail.com>
2018-07-05 21:38:32 +0100

    changed meta content

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,11 +8,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Basitceogren.com - Adım adım nasıl yapılır',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Teknoloji, hayat, spor, yemek ve birçok kategoride adım adım nasıl yapılır tavsiyeleri.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
